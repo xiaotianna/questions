d904290dc31300df421628d2967ee1a7dc0374f3
wifi歪f <1402772884@qq.com>
2025-09-05 18:08:24 +0800

    动态规划

--- docs/.vitepress/sidebar/index.ts
+++ docs/.vitepress/sidebar/index.ts
@@ -207,6 +207,7 @@ export const sidebar = {
         { text: '二叉树', link: '/算法/二叉树' },
         { text: '回溯', link: '/算法/回溯' },
         { text: '贪心', link: '/算法/贪心' },
+        { text: '动态规划', link: '/算法/动态规划' },
       ]
     }
   ],
